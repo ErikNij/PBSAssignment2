7f5ec786d1e0aba26fc8d63b70d77e01fea3d0e4
RL143 <r.l.m.lippens@student.tue.nl>
2023-10-03 09:59:18 +0200

    energy change

--- constants.h
+++ constants.h
@@ -1,11 +1,12 @@
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
-#include <math.h>
+//#include <math.h>
 
 /* Define constants for the entire program here */
 
 #define PI 3.141592653589
-#define KB (1.380649e-23) //J/K
-#define AV (6.022e23) 
+#define AV (6.022e23)       //1/mol
+#define Kb (1.38e-23)       // m2*kg-/s^2/K
+#define T 25                // celsius   
 
 #endif /* CONSTANTS_H_ */
