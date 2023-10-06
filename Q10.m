79f9ad1b3909bb0b5f98b1838a5fff5461c1fac0
RL143 <r.l.m.lippens@student.tue.nl>
2023-10-06 19:49:06 +0200

    Q10

---
+++ Q10.m
@@ -0,0 +1,16 @@
+% Read the data from the CSV file
+data = csvread('Velocity_Distribution.csv', 1, 0);
+
+% Extract velocity and probability values
+velocity = data(:, 1);
+probability = data(:, 2);
+
+% Plot the probability density function
+figure;
+plot(velocity, probability, 'LineWidth', 1.5);
+xlabel('Velocity');
+ylabel('Probability Density');
+title('Velocity Probability Density Function');
+grid on;
+
+% Add any additional customizations if needed
